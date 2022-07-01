be860cb61985d8a4cd7da5bea6ab73bdf4087876
haydar-c <82066544+haydar-c@users.noreply.github.com>
2022-07-01 09:19:40 +0300

    Update credentials_mywork.py

--- credentials_mywork.py
+++ credentials_mywork.py
@@ -4,15 +4,8 @@ Created on Sun Jun 26 14:01:58 2022
 
 @author: hayda
 """
-credentials = {
-    'user1':'pass1',
-    'user2':'pass2',
-    'user3':'pass3',
-    'haydar.cakan@ug.bilkent.edu.tr':'aselsan123',
-    'belediye@gmail.com':'belediye',
-    'test':'123',
-    'haydarcakan6@gmail.com':'123',
-    'tekirdag@gmail.com':'123',  
+credentials = {  
+    'a@test.com.tr':'123',  
     }
 
 # credentials[new_user]=new_user_password
